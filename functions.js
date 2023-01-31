61d27b32faaf5a6ba3a0f2ad840113ebd2705da8
Lukas Herrmann <lukas-oliver-ben.herrmann@iu-study.org>
2023-01-31 14:28:37 +0100

    Search Bar, Unterseiten fertig, Logo

--- functions.js
+++ functions.js
@@ -1,3 +1,15 @@
       $(document).ready(function () {
         $('#tabelle').DataTable();
     });
+
+
+    function search(){
+      let textToSearch = document.getElementById("search").value;
+      let paragraph = document.getElementById("paragraph");
+      textToSearch = textToSearch.replace(/[.*+?^${}()|[\]\\]/g,"\\$&");
+  
+      let pattern = new RegExp(`${textToSearch}`,"gi");
+  
+      paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`)
+  }
+
